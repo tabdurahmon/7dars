e8496181420e7662aa54e0136984498478590c66
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-07-01 20:32:33 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import Home from "./page/Home";
 import About from "./page/About";
 import Contact from "./page/Contact";
 import Navbar from "./components/Navbar";
+import MainLayout from "./layouts/MainLayout";
+import { ErrorPage, Login, Register } from "./page";
 
-export default function App() {
-  return <div>App</div>;
+function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      errorElement: <ErrorPage />,
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      errorElement: <ErrorPage />,
+      element: <Register />,
+    },
+  ]);
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function MainLayout() {
   return (
     <>
+      <Navbar />
       <main>
         <Outlet />
       </main>
